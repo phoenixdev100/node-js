16ac43514c8f7eeab6be4d6ee03cba63d1a07ed3
Deepak <deepakkumar312005@gmail.com>
2025-08-20 09:51:53 +0530

    Added previous weeks files

--- add.js
+++
@@ -1,9 +0,0 @@
-// function add(a, b) {
-//     return a + b
-// }
-
-const add = (a, b) => {
-    return a + b
-}
-
-module.exports = add
--- math.js
+++
@@ -1,3 +0,0 @@
-const add = require('./add')
-
-console.log('Sum of the given numbers are: ',add(2, 3))
